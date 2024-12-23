01510b57dbdc2f0bfe3943bf0296db45dac7341b
Akshay Saini <akshaymarch7@gmail.com>
2024-12-23 15:42:23 +0530

    Added .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://namastedev:gqipIs6ERtUYukAE@namastenode.rc211ms.mongodb.net/devTinder"
-  );
+  console.log(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports = connectDB;
